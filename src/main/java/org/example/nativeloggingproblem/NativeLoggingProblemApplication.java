7475a9cdbeed61a3676ef1538e5c08b46b5d0653
Hans Desmet <hans.desmet@vdabcampus.be>
2023-12-13 11:31:24 +0100

    initial

---
+++ src/main/java/org/example/nativeloggingproblem/NativeLoggingProblemApplication.java
@@ -0,0 +1,13 @@
+package org.example.nativeloggingproblem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NativeLoggingProblemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NativeLoggingProblemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/nativeloggingproblem/NativeLoggingProblemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.nativeloggingproblem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NativeLoggingProblemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
